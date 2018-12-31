0a16ab2bcfed9fed562c10032d450e05099bed2d
Jeff Thompson <jeffreykeatingthompson@gmail.com>
2018-12-30 18:09:00 -0800

    test change

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>
+        Open up App.js to start working on your app! My fist change.
+        </Text>
       </View>
     );
   }
