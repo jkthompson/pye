efe51a17c9746144aeadd549a4d92abf81cb29bf
Jeff <jeffreykeatingthompson@gmail.com>
2019-01-06 05:54:52 +0000

    isolate camera styles

--- app/components/Camera/Camera.js
+++ app/components/Camera/Camera.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import styles from './styles';
 
 export default class CameraExample extends React.Component {
   state = {
@@ -21,32 +22,10 @@ export default class CameraExample extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+        <View style={styles.container}>
+          <Camera style={styles.camera} type={this.state.type}>
             <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
+              style={styles.camera_view}>
             </View>
           </Camera>
         </View>
---
+++ app/components/Camera/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1
+  },
+  camera_view: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+});
